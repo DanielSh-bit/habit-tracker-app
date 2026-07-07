4af6a3102ec5814b81e912c0cc52d269c51e1008
Daniel_Shachak <daniel.shachak@gmail.com>
2026-07-07 12:51:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("האפליקציה נטענה בהצלחה");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then(function () {
+        console.log("Service Worker נרשם בהצלחה");
+      })
+      .catch(function (error) {
+        console.log("שגיאה ברישום Service Worker:", error);
+      });
+  });
+}
